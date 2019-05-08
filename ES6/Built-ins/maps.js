2ff7749fa01b3493645debe0e45e9f076d8ea408
budostylz <lewis.shaun1@outlook.com>
2019-05-08 18:57:28 -0400

    'updating_ES6_professional-developer-fu'

--- ES6/Built-ins/maps.js
+++ ES6/Built-ins/maps.js
@@ -1,70 +1,78 @@
+"use strict";
+
 //empty map
 //const employees = new Map();
 //console.log(employees);
-
 //set map objects
-const employees = new Map();
-
-employees.set('james.parkes@udacity.com', { 
-    firstName: 'James',
-    lastName: 'Parkes',
-    role: 'Content Developer' 
+var employees = new Map();
+employees.set('james.parkes@udacity.com', {
+  firstName: 'James',
+  lastName: 'Parkes',
+  role: 'Content Developer'
 });
 employees.set('julia@udacity.com', {
-    firstName: 'Julia',
-    lastName: 'Van Cleve',
-    role: 'Content Developer'
+  firstName: 'Julia',
+  lastName: 'Van Cleve',
+  role: 'Content Developer'
 });
 employees.set('richard@udacity.com', {
-    firstName: 'Richard',
-    lastName: 'Kalehoff',
-    role: 'Content Developer'
-});
-
-//console.log(employees);
-
-
+  firstName: 'Richard',
+  lastName: 'Kalehoff',
+  role: 'Content Developer'
+}); //console.log(employees);
 //delete employees
+
 employees.delete('julia@udacity.com');
-employees.delete('richard@udacity.com');
-
-//console.log(employees);
-
+employees.delete('richard@udacity.com'); //console.log(employees);
 //clear map
-employees.clear()
 
-//console.log(employees);
-
+employees.clear(); //console.log(employees);
 //check Map elements
-const members = new Map();
 
+var members = new Map();
 members.set('Evelyn', 75.68);
 members.set('Liam', 20.16);
 members.set('Sophia', 0);
-members.set('Marcus', 10.25);
-
-//console.log(members.has('Xavier'));
+members.set('Marcus', 10.25); //console.log(members.has('Xavier'));
 //console.log(members.has('Marcus'));
-
 //get value from key
 //console.log(members.get('Evelyn'));
-
 //using default iterator
-let iteratorObjForKeys = members.keys();
-//console.log( iteratorObjForKeys.next());
+
+var iteratorObjForKeys = members.keys(); //console.log( iteratorObjForKeys.next());
 //console.log( iteratorObjForKeys.next());
 //console.log( iteratorObjForKeys.next());
 
-let iteratorObjForValues = members.values();
+var iteratorObjForValues = members.values(); //console.log( iteratorObjForKeys.next());
 //console.log( iteratorObjForKeys.next());
 //console.log( iteratorObjForKeys.next());
-//console.log( iteratorObjForKeys.next());
-
 //using for...of Loop
-for (const member of members) {
-    //console.log(member);
+
+var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+  for (var _iterator = members[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+    var member = _step.value;
+  } //console.log(member);
+  //using forEach Loop
+
+} catch (err) {
+  _didIteratorError = true;
+  _iteratorError = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion && _iterator.return != null) {
+      _iterator.return();
+    }
+  } finally {
+    if (_didIteratorError) {
+      throw _iteratorError;
+    }
   }
+}
 
-//using forEach Loop
-members.forEach((value, key) => console.log(key, value));
-
+members.forEach(function (value, key) {
+  return console.log(key, value);
+});
--- ES6/Readme.md
+++ ES6/Readme.md
@@ -5,3 +5,20 @@ https://www.udacity.com/course/es6-javascript-improved--ud356
 ## JavaScript Promises
 https://www.udacity.com/course/javascript-promises--ud898
 
+## ECMAScript Compatibility Table
+http://kangax.github.io/compat-table/es6/
+
+## The Web is Eternal
+https://youtu.be/LwdL-8jFCyQ
+
+## Polyfills
+https://youtu.be/eC2_Umstx9U
+
+## What is a Polyfill?
+https://remysharp.com/2010/10/08/what-is-a-polyfill
+
+## Remy Sharp(Coiner of Polyfill)
+https://twitter.com/rem
+
+## Transpilling
+https://youtu.be/Ku2NATTmgks
