2d8297fa750d3ecdf96411d3787cdf68c12bfe3b
budostylz <lewis.shaun1@outlook.com>
2019-12-07 11:25:31 -0500

    update_object_oriented_javascript

---
+++ Object Oriented JavaScript/Objects in Depth/practice.js
@@ -0,0 +1,27 @@
+const mileTimes = [7.50, 6.25, 10.60, 8.88];
+
+console.log(mileTimes[mileTimes.length - 1])
+
+const populations = {
+    china: 1379000000,
+    brazil: 207700000,
+    india: 1324000000,
+    unitedStates: 323100000
+  };
+
+console.log(populations.brazil)
+
+const greetings = {
+    hello: {
+      english: 'hi',
+      french: 'bonjour',
+      portuguese: 'oi'
+    },
+    goodbye: {
+      english: 'bye',
+      french: 'au revoir',
+      portuguese: 'tchau'
+    }
+  };
+
+console.log(greetings.hello.portuguese)
